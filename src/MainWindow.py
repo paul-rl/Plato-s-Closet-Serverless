2c18720820f9606ae6294d11815624478c8b32e6
paul-rl <35239075+paul-rl@users.noreply.github.com>
2023-10-25 17:58:08 -0400

    Fixed cursor bug, added minor input validation

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -1,5 +1,5 @@
 from GUI import MainWindowGUI
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from dialogs.PasswordEntryDlg import PasswordEntryDlg
 from dialogs.EditTextDlg import EditTextDlg
 from dialogs.ExportDlg import ExportDlg
@@ -14,7 +14,8 @@ class MainWindow(QtWidgets.QMainWindow, MainWindowGUI.Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
 
-        # TODO: Don't recreate tables every time
+        # Start our database
+        # TODO: Make database run independently
         self.connection = database.connect()
         database.create_table(self.connection)
 
@@ -22,6 +23,16 @@ class MainWindow(QtWidgets.QMainWindow, MainWindowGUI.Ui_MainWindow):
         self.editTextButton.clicked.connect(self.editTextMessage)
         self.exportButton.clicked.connect(self.exportPhoneRegistry)
         self.submitButton.clicked.connect(self.registerNumber)
+
+        # Set event filter for vendor # cursor position
+        self.VendorPhoneNoInput.installEventFilter(self)
+    
+    def eventFilter(self, source, event):
+        if source == self.VendorPhoneNoInput and event.type() == QtCore.QEvent.MouseButtonPress:
+            self.VendorPhoneNoInput.setFocus(QtCore.Qt.MouseFocusReason)
+            self.VendorPhoneNoInput.setCursorPosition(0)
+            return True
+        return super().eventFilter(source, event)
 
     # Function called when the edit text message button is clicked. On click,
     # prompts the user to enter a password. On password success, the text edit
@@ -31,29 +42,48 @@ class MainWindow(QtWidgets.QMainWindow, MainWindowGUI.Ui_MainWindow):
         if passwordCorrect:
             self.openEditTextDialog()
 
+    # Function called after successful password input. The text message sent to 
+    # users can be edited from the provided dialog
     def openEditTextDialog(self):
         dlg = EditTextDlg(self)
         dlg.exec()
 
-    def openExportDialog(self):
-        dlg = ExportDlg(self)
-        dlg.exec()
-
+    # Function called when the export phone registry button is clicked.
+    # On click, prompts the user to enter a password. On a success,
+    # the export dialog pops up. On a failure, the password dialog closes.
     def exportPhoneRegistry(self):
         passWordCorrect = self.openPasswordDialog()
         if passWordCorrect:
             self.openExportDialog()
 
+    # Function called after successful password input. Allows exporting
+    # database information.
+    def openExportDialog(self):
+        dlg = ExportDlg(self)
+        dlg.exec()
+
     def registerNumber(self):
         # Remove all non-numeric characters from phone number
-        vendorPhoneNo = self.VendorPhoneNoInputField.text()
+        vendorPhoneNo = self.VendorPhoneNoInput.text()
         vendorPhoneNo = ''.join(c for c in vendorPhoneNo if c.isdigit())
+        orderNo = self.orderNoInput.text()
+
+        # Clear input fields
+        self.VendorPhoneNoInput.setText("")
+        self.orderNoInput.setText("")
         
-        orderNo = 0
-        if database.contains(self.connection, vendorPhoneNo, orderNo):
-            self.phoneFoundDialog(vendorPhoneNo, orderNo)
+        #TODO: Make this validation more rigorous
+        if len(vendorPhoneNo) == 10 and len(orderNo) >= 1:
+            if database.contains(self.connection, vendorPhoneNo, orderNo):
+                self.phoneFoundDialog(vendorPhoneNo, orderNo)
+            else:
+                self.phoneNotFoundDialog(vendorPhoneNo, orderNo)
         else:
-            self.phoneNotFoundDialog(vendorPhoneNo, orderNo)
+            print("Please provide a valid input")
+            # One of a couple of options..
+            # 1. Please enter a valid phone number
+            # 2. Please eneter a valid order number
+            # 3. The order number you entered does not match with this phone
 
     def phoneFoundDialog(self, phoneNo, orderNo):
         dlg = PhoneFoundDlg(phoneNo, orderNo, self)
