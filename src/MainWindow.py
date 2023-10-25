f0ca5f46fc37ac85a0414140674a1ad73ea50769
paul-rl <35239075+paul-rl@users.noreply.github.com>
2023-10-24 20:45:32 -0400

    Connected buttons to functions

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -5,3 +5,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
+
+        # Connect buttons
+        self.editTextButton.clicked.connect(self.editTextMessage)
+        self.exportButton.clicked.connect(self.exportPhoneRegistry)
+        self.submitButton.clicked.connect(self.registerNumber)
+
+    def editTextMessage(self):
+        print("Edit text message")
+
+    def exportPhoneRegistry(self):
+        print("Export Phone Registry")
+
+    def registerNumber(self):
+        print("Registering")
