9bcc831644969a3d5b904c09c134ea4f67085c22
paul-rl <35239075+paul-rl@users.noreply.github.com>
2023-10-25 17:06:18 -0400

    passed phone # and order # to phone search dialogs

--- src/dialogs/PhoneFoundDlg.py
+++ src/dialogs/PhoneFoundDlg.py
@@ -5,14 +5,17 @@ from PyQt5.QtWidgets import QDialog
 
 class PhoneFoundDlg(QDialog):
     ''' Phone Found dialog '''
-    def __init__(self, parent=None):
+    def __init__(self, phoneNo, orderNo, parent=None):
         super().__init__(parent)
         self.ui = PhoneFoundGUI.Ui_Dialog()
         self.ui.setupUi(self)
+        self.inputtedPhoneNo = phoneNo
+        self.inputtedOrderNo = orderNo
         
         # Connect buttons
         self.ui.yesButton.clicked.connect(self.sendText)
         self.ui.noButton.clicked.connect(self.close)
 
     def sendText(self):
-        print("Sent Text")
+        print("Sent Text to " + self.inputtedPhoneNo)
+        self.close()
--- src/dialogs/PhoneNotFoundDlg.py
+++ src/dialogs/PhoneNotFoundDlg.py
@@ -1,18 +1,23 @@
 from pathlib import Path
 from GUI import PhoneNotFoundGUI
 from PyQt5.QtWidgets import QDialog
-
+import database
+import datetime
 
 class PhoneNotFoundDlg(QDialog):
     ''' Phone Found dialog '''
-    def __init__(self, parent=None):
+    def __init__(self, phoneNo, orderNo, parent=None):
         super().__init__(parent)
         self.ui = PhoneNotFoundGUI.Ui_Dialog()
         self.ui.setupUi(self)
+        self.inputtedPhoneNo = phoneNo
+        self.inputtedOrderNo = orderNo
 
         # Connect buttons
         self.ui.yesButton.clicked.connect(self.addToDb)
         self.ui.noButton.clicked.connect(self.close)
-
+        
     def addToDb(self):
-        print("Added to db")
+        connection = database.connect()
+        database.addEntry(connection, datetime.datetime.now(), phoneNo=self.inputtedPhoneNo, orderNo=self.inputtedOrderNo)
+        self.close()
