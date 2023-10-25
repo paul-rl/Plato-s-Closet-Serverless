beb59b5943e0a19bdcf152e5f38c2afee9ade295
paul-rl <35239075+paul-rl@users.noreply.github.com>
2023-10-25 19:12:33 -0400

    Added export functionality

--- src/dialogs/ExportDlg.py
+++ src/dialogs/ExportDlg.py
@@ -1,23 +1,43 @@
 from GUI import ExportGUI
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
+import database
+import csv
+from pathlib import Path
 
 class ExportDlg(QDialog):
     ''' Export dialog '''
-    def __init__(self, parent=None):
+    def __init__(self, connection, parent=None):
         super().__init__(parent)
         self.ui = ExportGUI.Ui_Dialog()
         self.ui.setupUi(self)
+        self.dbConnection = connection
 
         # Connect buttons
         self.ui.exportButton.clicked.connect(self.export)
 
-    # TODO: We are exporting
     def export(self):
+        # Parse data
         fromDate = self.ui.fromDateField.date().toPyDate()
         toDate = self.ui.toDateField.date().toPyDate()
         phoneNo = self.ui.phoneNoField.text()
-
-        #database.query()
         
-        print("Exported!")
-        self.close()
+        results = database.query(self.dbConnection, fromDate, toDate, phoneNo)
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getSaveFileName(self, 'Save File', '', 'CSV Files(*.csv)', options=options)
+        
+        if file_name:
+            f = open(file_name, 'w', newline='')
+            csvWriter = csv.writer(f)
+            for row in results:
+                csvWriter.writerow(row)
+            
+            self.setWindowTitle(str(Path().parent.absolute() / file_name) + " - Phone Registry")
+            f.close()
+            self.close()
+            print("Exported!")
+            return True
+        else:
+            #TODO: There was an error exporting your file
+            self.close()
+            return False       
+        
