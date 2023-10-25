e610ef9280586d5b417622510a6823bd97f75847
paul-rl <35239075+paul-rl@users.noreply.github.com>
2023-10-24 22:07:58 -0400

    Added basic password verification to dialog

--- src/PasswordEntryDlg.py
+++ src/PasswordEntryDlg.py
@@ -8,3 +8,14 @@ class PasswordEntryDlg(QDialog):
         super().__init__(parent)
         self.ui = PasswordEntryGUI.Ui_Dialog()
         self.ui.setupUi(self)
+
+        # TODO: CHANGE PASSWORD IMPLEMENTATION
+        self.password = "123"
+        self.passwordCorrect = False
+        # Connect submit button
+        self.ui.submitButton.clicked.connect(self.verifyPassword)
+   
+    def verifyPassword(self):
+        self.passwordCorrect = self.ui.passwordField.text() == "123"
+        if (self.passwordCorrect):
+            self.close()
